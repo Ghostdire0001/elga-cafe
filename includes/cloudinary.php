02e6d8162ff494699e868ba3182e8e610d85292b
Ghostdire0001 <136738145+Ghostdire0001@users.noreply.github.com>
2026-04-13 20:50:54 +0300

    Delete includes/cloudinary.php

--- includes/cloudinary.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Cloudinary configuration
-define('CLOUDINARY_CLOUD_NAME', 'dkktgr8uw');
-define('CLOUDINARY_API_KEY', '592375197738879');
-define('CLOUDINARY_API_SECRET', '3QmaxyvvD_os8x6KV255Du8XXZs');
-
-// You can also set these as environment variables on Render
-// For now, replace the values above with your actual Cloudinary credentials
-?>
