208a3311b949e6ab6e0496302a9066139e55febb
Ghostdire0001 <136738145+Ghostdire0001@users.noreply.github.com>
2026-04-17 23:03:48 +0300

    Implement language selector HTML generation
    Added a language selector function to generate HTML for language options.

--- includes/language.php
+++ includes/language.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/translations.php';
 
 function getCurrentLanguage() {
-    // Check URL parameter first
+    // Check URL parameter first (handled at top of index.php)
     if (isset($_GET['lang'])) {
         return $_GET['lang'];
     }
@@ -29,5 +29,22 @@ function t($key) {
     }
     
     return $translations[$current_lang][$key] ?? $key;
+}
+
+function getLanguageSelectorHTML() {
+    global $available_languages, $current_lang;
+    
+    if (!isset($current_lang)) {
+        $current_lang = getCurrentLanguage();
+    }
+    
+    $html = '<select id="language-selector" class="lang-selector" onchange="changeLanguage(this.value)">';
+    foreach ($available_languages as $code => $name) {
+        $selected = $current_lang == $code ? 'selected' : '';
+        $html .= "<option value=\"$code\" $selected>$name</option>";
+    }
+    $html .= '</select>';
+    
+    return $html;
 }
 ?>
