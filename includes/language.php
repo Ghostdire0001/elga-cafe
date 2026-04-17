3de6fd342fc18afb087cb6a0ab5ae4c0436f3e00
Ghostdire0001 <136738145+Ghostdire0001@users.noreply.github.com>
2026-04-17 21:44:40 +0300

    Refactor language functions and remove unused code
    Refactor language handling functions by removing unused functions and simplifying language retrieval.

--- includes/language.php
+++ includes/language.php
@@ -2,11 +2,9 @@
 require_once __DIR__ . '/translations.php';
 
 function getCurrentLanguage() {
-    // Check URL parameter first (highest priority)
+    // Check URL parameter first
     if (isset($_GET['lang'])) {
-        $lang = $_GET['lang'];
-        setLanguageCookie($lang);
-        return $lang;
+        return $_GET['lang'];
     }
     
     // Then check cookie
@@ -23,48 +21,13 @@ function getCurrentLanguage() {
     return 'am';
 }
 
-function setLanguageCookie($lang) {
-    setcookie('user_lang', $lang, time() + (86400 * 30), "/", "", false, true);
-    $_SESSION['user_lang'] = $lang;
-}
-
 function t($key) {
     global $translations, $current_lang;
     
-    // If current_lang is not set in global scope, get it
     if (!isset($current_lang)) {
         $current_lang = getCurrentLanguage();
     }
     
-    // Return translation if exists, otherwise return the key
     return $translations[$current_lang][$key] ?? $key;
-}
-
-function getLanguageSelectorHTML() {
-    global $available_languages, $current_lang;
-    
-    if (!isset($current_lang)) {
-        $current_lang = getCurrentLanguage();
-    }
-    
-    $html = '<select id="language-selector" class="lang-selector" onchange="changeLanguage(this.value)">';
-    foreach ($available_languages as $code => $name) {
-        $selected = $current_lang == $code ? 'selected' : '';
-        $html .= "<option value=\"$code\" $selected>$name</option>";
-    }
-    $html .= '</select>';
-    
-    return $html;
-}
-
-function getLanguageScript() {
-    return '
-    <script>
-        function changeLanguage(lang) {
-            const url = new URL(window.location.href);
-            url.searchParams.set("lang", lang);
-            window.location.href = url.toString();
-        }
-    </script>';
 }
 ?>
